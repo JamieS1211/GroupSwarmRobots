775d9906f6d4bca8be349fe82612dc97957848ee
WillRicey <60102717+WillRicey@users.noreply.github.com>
2020-02-07 21:56:36 +0000

    Creation of compass files

---
+++ compass.c
@@ -0,0 +1,46 @@
+/*
+ * File:   compass.c
+ * Author: willrice
+ *
+ * Created on January 26, 2020, 2:05 PM
+ */
+
+
+#include <xc.h> // Think is library type thing
+
+#include "globals.h"
+#include "compass.h"
+
+//Entire HMC5883L Compass Manual
+
+/*
+ * Compass Setup - Page 12 - 14
+ * Individual data sheet
+ * Setting up single measure mode
+ */
+void comp_Setup(void);
+
+
+
+/*
+ * Compass Testing+Calibration - Page 19
+ * Removing natural offset by applying 1.1 Gauss field
+ * Sets gain to correct value
+ */
+void comp_Test(void, void, void);
+
+
+
+/*
+ * Compass Testing+Calibration - Page 15, 18
+ * Register Document
+ * Returns Compass heading
+ */
+void comp_Head(void, void, void);
+
+/*
+ * Compass Testing+Calibration - Page 15, 18
+ * Register Document
+ * Returns Compass heading in Gauss
+ */
+void comp_Raw(void, void, void);
