e2b056479ef8578c983d981c596045ac57db5fbf
WillRicey <60102717+WillRicey@users.noreply.github.com>
2020-02-11 12:59:38 +0000

    ADC fix and config file

--- ADC.c
+++ ADC.c
@@ -80,9 +80,9 @@ int ADC_Read(int x) {
     
     while (ADCON0bits.GO); // Wait until ADC conversion completes
     ADCON0bits.ON = 0; // Turn back off ADC
-    if (RA3 = High){
-        RA3 = Low; // ---------- Fix; Toggle solar isolation pin --------
-    }
+//    if (RA3 = High){
+//        RA3 = Low; // ---------- Fix; Toggle solar isolation pin --------
+//    }
     return (ADRESH << 4) + ADRESL;
 }
 
