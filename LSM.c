fa8e68147f9d76bc06a63152eee6587e2194a981
Oliver <59800910+crabrace@users.noreply.github.com>
2020-02-24 13:47:58 +0000

    Update LSM with correct motor functions

--- LSM.c
+++ LSM.c
@@ -16,69 +16,34 @@
 
 // Deal with all light source management scripts
 bool PowerFinding(void){
-    //When battery is low check if good source in memory and nearby
-    // if Solar voltage < threshold{
-        // Read in environment memory
     
-        // Check for location
-        //If distance & Uncertainty < thresholds {
-            //Call distance closing function}
-        //else {
-            // int count = 0;
-            // While solarV < threshold & Iterations < max -  Repeat all up to max iterations or until solar threshold voltage is reached
-    
-            //Read ADC for all three sensors
-            // int x0 = 0
-            // int x1 = LDR Front
-            //while x1 > x0
-            // turn then x0= x1, then x1 = scan
-            //Turn one step back after to undo difference 
-            // While x1 > x0
-            // Turn other way, x0 = x1, then x1 = scan
-            //Turn back one step after to undo difference
-            //Drive straight, check solar cell in intervals. Once it starts to drop again and two LDR are near balanced stop.
-            // count = count + 1; Iterate count
-    
-    
-    // else {
-    //  sit and charge
-    //}
-
-    // If no source found check sensors - search until reasonable signal detected
-
-
-    //Move in straight lines - Rapid LDR decline and similarity when perpendicular
-
-    // Repeat until threshold solar voltage is reached 
-
-    
-    if (ADC_Read(2) < 600){ // ------------Check how this works as binary passed---------
+    //if (ADC_Read(2) < 600){ // ------------Check how this works as binary passed---------
         //if (knownSourceDistance < Threshold & sourceUncertainty < Threshold2){
-            //move(requiredTurn(sourceAngle), sourceDistance)
+        //    move(requiredTurn(sourceAngle), sourceDistance)
         //}
         //else {
             
         //}
-    }
+    //}
     int iteration = 0;
     while (ADC_Read(2) < 800) {
         while (ADC_Read(2) < 600) {
             // Long range closing use LDR's
             int previous = ADC_Read(0);
-            rotate(M_PI/6);
+            motor_spin(M_PI/6);
             while (ADC_Read(0)< previous){
-                rotate(M_PI/6);
+                motor_spin(M_PI/6);
                 previous = ADC_Read(0);
             }
-            rotate(-M_PI/6);
+            motor_spin(-M_PI/6);
             while (ADC_Read(0)< previous){
-                rotate(-M_PI/6);
+                motor_spin(-M_PI/6);
                 previous = ADC_Read(0);
             }
-            rotate(-M_PI/6);
+            motor_spin(-M_PI/6);
 
             while (ADC_Read(0)<ADC_Read(1)){
-                move(0, 5);
+                motor_spin(0, 5);
             }
         }
         //Short range closing using Solar
@@ -90,12 +55,12 @@ bool PowerFinding(void){
         // --------   Check for improvement not for ultimate value
         
         while (iteration < 12){
-            move(M_PI/6, 1);
+            move_dist(M_PI/6, 1, 0); 
             if (ADC_Read(2) > 800){
                 return true;
             }
-            move(M_PI,1);
-            move(M_PI,0);
+            move_dist(M_PI,1, 0);
+            move_dist(M_PI,0, 0);
             iteration +=1;
         }
         
@@ -107,7 +72,6 @@ bool PowerFinding(void){
 }
 
 
-//
 
 
 float requiredTurn(float angle){
