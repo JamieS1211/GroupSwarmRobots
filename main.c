0dfef1cb3a0981b422ac0347d841aa04d15593a0
Oliver <59800910+crabrace@users.noreply.github.com>
2020-02-27 16:28:03 +0000

    Revert "Merge branch 'WorkingADC' into CodeGenGen"
    This reverts commit 63eda6046fa5936a6757bee22e6e9692d92ab351, reversing
    changes made to 055d3d0f01aff3843448d4600fc4defa74c1fff3.

--- main.c
+++ main.c
@@ -15,14 +15,6 @@
 #pragma warning disable 520
 #pragma warning disable 2053
 #pragma warning disable 1498
-#include <xc.h>
-#include "globals.h"
-#include "interrupts.h"
-#include "i2c.h"
-#include "testing.h"
-#include "Collision.h"
-#include "Lidar.h"
-#include "ADC.h"
 
 #include "mcc_generated_files/device_config.h"
 #include "mcc_generated_files/mcc.h"
@@ -45,25 +37,37 @@ void main(void) {
     //VL53L0X_Setup(0x00);
     VL53L0X_Setup(0x29);
     
-    while(1){
-        uint16_t value = ADC_Read(0);
-        if (value > 1000){
-            TRISAbits.TRISA5 = 0;
-            LATAbits.LATA5 = 1;
-        }
-        else{
-            TRISAbits.TRISA5 = 0;
-            LATAbits.LATA5 = 0;
-        }
-        uint16_t value2 = ADC_Read(1);
-        if (value2 > 1000){
-            TRISAbits.TRISA6 = 0;
-            LATAbits.LATA6 = 1;
-        }
-        else{
-            TRISAbits.TRISA6 = 0;
-            LATAbits.LATA6 = 0;
-        }
+    while(1) {
+        __delay_ms(100);
+    uint16_t value =0;
+    value = VL53L0X_ReadRange(0x29);
+    
+    if (value > 200 && value < 400) {
+        TRISAbits.TRISA4 = 0;
+        LATAbits.LATA4 = 1;
+    }
+    else {
+        LATAbits.LATA4 =0;
+    }
+    
+    __delay_ms(200);
+    char string[1] = "i";
+    cereal_str(string);
+    /* 
+     * if (value > 500) {
+        TRISAbits.TRISA5 = 0;
+        TRISAbits.TRISA5 = 1;
+    }
+     */
+        
+        
+        /*
+        while(!UART2_is_tx_ready());
+        UART2_Write((int)'H');
+        UART2_Write(0x69);
+        UART2_Write(0x21);
+        UART2_Write(0x20);
+          */
     }
     
     return;
