84e739207951cb7744a2648339d28824a6b69af3
Oliver <59800910+crabrace@users.noreply.github.com>
2020-02-27 17:16:20 +0000

    Update Pin Map

--- main.c
+++ main.c
@@ -12,22 +12,52 @@
  * PIC18(L)F26/27/45/46/47/55/56/57K42
  */
 
+/*
+ * Pin map with how used
+ * 
+ * 1  - VPP/MCLR/RE3                - [PROGRAMMER VPP]
+ * 2  - RA0                         - [LDR ADC]
+ * 3  - RA1                         - [LDR 2 ADC]
+ * 4  - RA2                         - [Solar ADC]
+ * 5  - RA3                         - [Solar Isolation toggle]
+ * 6  - RA4                         - [ADC Toggle]
+ * 7  - RA5                         - [NONE]
+ * 8  - VSS                         - [+ 0V]
+ * 9  - RA7                         - [XSHUT 1]
+ * 10 - RA6                         - [XSHUT 2]
+ * 11 - RC0                         - [NONE]
+ * 12 - RC1                         - [NONE] ?      THE BEN ZONE
+ * 13 - RC2                         - [NONE] ?      ------------
+ * 14 - RC3                         - [SCK1 SPI] ?  ------------
+ * 15 - RC4                         - [SDI1 SPI] ?  ------------
+ * 16 - RC5                         - [NONE] ?      ------------
+ * 17 - RC6                         - [NONE] ?      ------------
+ * 18 - RC7                         - [NONE]
+ * 19 - VSS                         - [+ 0V]
+ * 20 - VDD                         - [+ 5V]
+ * 21 - RB0                         - [GPI0]
+ * 22 - RB1                         - [SDO1]
+ * 23 - RB2                         - [SDI1 - SPI]
+ * 24 - RB3                         - [SCK1 - SPI]
+ * 25 - RB4                         - [SDA2 - I2C]
+ * 26 - RB5                         - [SCL2 - I2C]
+ * 27 - RB6/ICSPCLK                 - [TX2] THERE IS NO TX1
+ * 28 - RB7/ICSPDAT                 - [RX2]
+ */
+
 #pragma warning disable 520
 #pragma warning disable 2053
 #pragma warning disable 1498
 #include <xc.h>
 #include "globals.h"
-#include "interrupts.h"
-#include "i2c.h"
-#include "testing.h"
 #include "Collision.h"
-#include "Lidar.h"
 #include "ADC.h"
 
 #include "mcc_generated_files/device_config.h"
 #include "mcc_generated_files/mcc.h"
 #include "VL53L0X.h"
 #include "cereal.h"
+#include "compass.h"
 
 void main(void) {
     SYSTEM_Initialize();
@@ -45,6 +75,7 @@ void main(void) {
     //VL53L0X_Setup(0x00);
     VL53L0X_Setup(0x29);
     
+    // While loop testing ADC then Lidar then compass
     while(1){
         uint16_t value = ADC_Read(0);
         if (value > 1000){
@@ -63,6 +94,20 @@ void main(void) {
         else{
             TRISAbits.TRISA6 = 0;
             LATAbits.LATA6 = 0;
+        }
+        __delay_ms(100);
+        uint16_t pizza = VL53L0X_ReadRange(0x29);
+        __delay_ms(200);
+        comp_reset();
+        float ang = comp_head();
+        
+        if (ang>1){
+            TRISAbits.TRISA7 = 0;
+            LATAbits.LATA7 = 1;
+        }
+        else {
+            TRISAbits.TRISA7 = 0;
+            LATAbits.LATA7 = 0;
         }
     }
     
